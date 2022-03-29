9a0027db40613ce625fb21b9aaa0ad26e6e3cf9e
unknown <tu7d5w0m5@code.edu.az>
2022-03-29 00:17:16 -0800

    first commit

---
+++ ExtensionMethod.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _29032022task1
+{
+    static class ExtensionMethod
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 != 0)
+                return true;
+
+            return false;
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0)
+                return true;
+            return false;
+
+        }
+        public static bool IsContainsDigit(this string str, char Check)
+        {
+            foreach (var item in str)
+            {
+                if (item == Check)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        public static string ToCapitalize(this string text)
+        {
+            text = text.ToLower();
+            string[] promises = text.Split(' ');
+            string newtext = "";
+            foreach (var promise in promises)
+            {
+                string newPromise = char.ToUpper(promise[] + promise.Substring(1) + " ");
+                newtext = newtext + newPromise;
+            }
+            return newtext;
+        }
+        public static int [] GetValueIndexes(this string str,char letter)
+        {
+            int[] index = new int[0];
+            for (int i = 0; i <str.Length; i++)
+            {
+                if (str[i] == letter) 
+                {
+                    Array.Resize(ref index, index.Length + 1);
+                    index[index.Length - 1] = i;
+                }
+            }
+            return index;
+
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _29032022task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+           Console.WriteLine(number.IsOdd());
+            Console.WriteLine(number.IsEven());
+            string promise = "Proqram isledi";
+            Console.WriteLine(promise.IsContainsDigit('a'));
+            Console.ReadLine();
+        }
+    }
+}
